faacdf73e4f28831331e6ad9c45e041d074fcb60
SaltyF1sh312 <64980287+SaltyF1sh312@users.noreply.github.com>
2020-07-19 12:54:31 +0800

    Update Population.java

--- geneticAlgorithm/Population.java
+++ geneticAlgorithm/Population.java
@@ -1,6 +1,11 @@
 package geneticAlgorithm;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Population {
 	/**
@@ -14,174 +19,111 @@ public class Population {
 	/**
 	 * The rate of individuals crossing
 	 */
-	private final static double crossRate = 0.6;
+	private final static double crossOverRate = 0.6;
+
 	/**
 	 * Construct a new <code>Population</code> object with default properties
 	 */
-	public Population() {
-		this(1, Arrays.asList(new Individual()));
+	Population() {
+
 	}
+
 	/**
-	 * Construct a new <code>Population</code> object with the populationSize and the individuals
+	 * Construct a new <code>Population</code> object with the populationSize and
+	 * the individuals
+	 * 
 	 * @param populationSize
 	 * @param individuals
 	 */
-	public Population(int populationSize, List<Individual> individuals) {
-		this.populationSize = populationSize;
+	Population(List<Individual> individuals) {
 		this.individuals = individuals;
+		this.populationSize = individuals.size();
 	}
+
 	/**
 	 * Get the populationSize of the <code>Population</code>
+	 * 
 	 * @return the populationSize of the <code>Population</code>
 	 */
-	public int getPopulationSize() {
+	int getPopulationSize() {
 		return populationSize;
 	}
+
 	/**
 	 * Get the individuals of the <code>Population</code>
+	 * 
 	 * @return the individuals of the <code>Population</code>
 	 */
-	public List<Individual> getIndividuals() {
+	List<Individual> getIndividuals() {
 		return individuals;
 	}
+
 	/**
 	 * Set the populationSize of the <code>Population</code>
+	 * 
 	 * @param populationSize
 	 */
-	public void setPopulationSize(int populationSize) {
+	void setPopulationSize(int populationSize) {
 		this.populationSize = populationSize;
 	}
-	/**
-	 * Set the individuals of the <code>Population</code>
-	 * @param individuals
-	 */
-	public void setIndividuals(List<Individual> individuals) {
-		this.individuals = individuals;
-	}
+
 	/**
 	 * Add the individual to the <code>Population</code>
+	 * 
 	 * @param individual
 	 */
-	public void addIndividual(Individual individual) {
+	void addIndividual(Individual individual) {
 		individuals.add(individual);
 	}
-	/**
-	 * Remove the individual from the <code>Population</code>
-	 * @param individual
-	 */
-	public void removeIndividual(Individual individual) {
-		if (individuals.contains(individual))
-			individuals.remove(individual);
-		else
-			throw new NullPointerException("this individual doesn't exist!");
-	}
+
 	/**
 	 * <code>Individual</code> in the <code>Population</code> cross
 	 */
-	public void cross() {
+	void crossOver() {
 		for (int i = 0; i < populationSize; i++) {
-			if (Math.random() < crossRate) {
-				String child1 = "", child2 = "";
+			if (Math.random() < crossOverRate) {
+				String firstChild = "", secondChild = "";
 				int other;
 				// Two chromosomes should be different
-				while ((other = new Random().nextInt(populationSize)) == i) continue;
+				while ((other = new Random().nextInt(populationSize)) == i)
+					continue;
 				// The position where two chromosomes cross
 				int position = new Random().nextInt(individuals.get(i).getChromosome().getGeneLength() - 1) + 1;
-				
+
 				// cross two random chromosomes at the position
-				child1 = individuals.get(i).getChromosome().getSequence().substring(0, position) + 
-						individuals.get(other).getChromosome().getSequence().substring(position);
-				child2 = individuals.get(other).getChromosome().getSequence().substring(0, position) + 
-						individuals.get(i).getChromosome().getSequence().substring(position);
-				addIndividual(new Individual(new Chromosome(child1)));
-				addIndividual(new Individual(new Chromosome(child2)));
+				firstChild = individuals.get(i).getChromosome().getSequence().substring(0, position)
+						+ individuals.get(other).getChromosome().getSequence().substring(position);
+				secondChild = individuals.get(other).getChromosome().getSequence().substring(0, position)
+						+ individuals.get(i).getChromosome().getSequence().substring(position);
+				addIndividual(new Individual(new Chromosome(firstChild)));
+				addIndividual(new Individual(new Chromosome(secondChild)));
 			}
 		}
 	}
+
 	/**
-	 * Get the <code>Individual</code> who has the minimum fitness
-	 * @return the <code>Individual</code> who has the minimum fitness
+	 * Get the first <code>Individual</code>
+	 * 
+	 * @return the first <code>Individual</code>
 	 */
-	public Individual getMinFitness() {
-		Individual min = individuals.get(0);
-		for (Individual individual: individuals)
-			if (individual.getFitness() < min.getFitness())
-				min = individual;
-		
-		return min;
+	Individual getAlpha() {
+		return individuals.get(0);
 	}
+
 	/**
-	 * Get the <code>Individual</code> who has the maximum fitness
-	 * @return the <code>Individual</code> who has the maximum fitness
+	 * Select individuals whose fitness are lower
 	 */
-	public Individual getMaxFitness() {
-		Individual max = individuals.get(0);
-		for (Individual individual: individuals)
-			if (individual.getFitness() > max.getFitness())
-				max = individual;
-		
-		return max;
+	void minSelect() {
+		individuals = individuals.stream().sorted(Comparator.comparingDouble(Individual::getFitness))
+				.limit(populationSize).collect(Collectors.toList());
 	}
+
 	/**
-	 * Get the <code>Individual</code> who has the fitness closest to the target
-	 * @return the <code>Individual</code> who has the fitness closest to the target
+	 * Select individuals whose fitness are higher
 	 */
-	public Individual getBestFitness() {
-		Individual best = individuals.get(0);
-		for (Individual individual: individuals)
-			if (Math.abs(individual.getFitness() - TestGeneticAlgorithm.TARGET) < 
-					Math.abs(best.getFitness() - TestGeneticAlgorithm.TARGET))
-				best = individual;
-		
-		return best;
-	}
-	/**
-	 * Select individuals who have a lower fitness
-	 */
-	public void minSelect() {
-		if (individuals.size() == populationSize) // There are no individuals in the <code>Population</code> crossing
-			return;
-		else {
-			List<Individual> individuals = new ArrayList<Individual>();
-			for (int i = 0; i < populationSize; i++) {
-				Individual minFitness = this.getMinFitness();
-				this.individuals.remove(minFitness);
-				individuals.add(minFitness);
-			}
-			setIndividuals(individuals);
-		}
-	}
-	/**
-	 * Select individuals who have a higher fitness
-	 */
-	public void maxSelect() {
-		if (individuals.size() == populationSize) // There are no individuals in the <code>Population</code> crossing
-			return;
-		else {
-			List<Individual> individuals = new ArrayList<Individual>();
-			for (int i = 0; i < populationSize; i++) {
-				Individual maxFitness = this.getMaxFitness();
-				this.individuals.remove(maxFitness);
-				individuals.add(maxFitness);
-			}
-			setIndividuals(individuals);
-		}
-	}
-	/**
-	 * Select individuals who have a fitness closer to the target
-	 */
-	public void targetSelect() {
-		if (individuals.size() == populationSize) // There are no individuals in the <code>Population</code> crossing
-			return;
-		else {
-			List<Individual> individuals = new ArrayList<Individual>();
-			for (int i = 0; i < populationSize; i++) {
-				Individual bestFitness = this.getBestFitness();
-				this.individuals.remove(bestFitness);
-				individuals.add(bestFitness);
-			}
-			setIndividuals(individuals);
-		}
+	void maxSelect() {
+		Collections.sort(individuals, Comparator.reverseOrder());
+		individuals = individuals.stream().limit(populationSize).collect(Collectors.toList());
 	}
 
 }
