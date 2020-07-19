ab2d31fa05a72c931e7a7ed184ea65a72d6e6ae7
SaltyF1sh312 <64980287+SaltyF1sh312@users.noreply.github.com>
2020-07-19 12:52:30 +0800

    Update Individual.java

--- geneticAlgorithm/Individual.java
+++ geneticAlgorithm/Individual.java
@@ -2,7 +2,7 @@ package geneticAlgorithm;
 
 import java.util.Random;
 
-public class Individual {
+public class Individual implements Comparable<Individual> {
 	/**
 	 * The chromosome of the individual
 	 */
@@ -10,44 +10,51 @@ public class Individual {
 	/**
 	 * The value after putting chromosome into the target function
 	 */
-	private double fitness;
+	private Double fitness;
 	/**
 	 * The rate of gene mutating
 	 */
 	private final static double mutateRate = 0.01;
-	
+
 	/**
 	 * Construct a new <code>Individual</code> object with default properties
 	 */
-	public Individual() {
-		this(new Chromosome());
+	Individual() {
+
 	}
+
 	/**
 	 * Construct a new <code>Individual</code> object with the chromosome
+	 * 
 	 * @param chromosome
 	 */
-	public Individual(Chromosome chromosome) {
+	Individual(Chromosome chromosome) {
 		this.chromosome = chromosome;
 		this.fitness = TestGeneticAlgorithm.getFunctionValue(chromosome.getDecimalX(), chromosome.getDecimalY());
 	}
+
 	/**
 	 * Get the chromosome of the <code>Individual</code>
+	 * 
 	 * @return the chromosome of the <code>Individual</code>
 	 */
-	public Chromosome getChromosome() {
+	Chromosome getChromosome() {
 		return chromosome;
 	}
+
 	/**
 	 * Get the fitness of the <code>Individual</code>
+	 * 
 	 * @return the fitness of the <code>Individual</code>
 	 */
-	public double getFitness() {
+	Double getFitness() {
 		return fitness;
 	}
+
 	/**
 	 * Chromosome of this mutates
 	 */
-	public void mutate() {
+	void mutate() {
 		if (Math.random() < mutateRate) {
 			String mutation = ""; // Part of the sequence after mutation
 			String temp = "";
@@ -55,36 +62,41 @@ public class Individual {
 			// How many sequences this can mutate
 			int mutateInterval = new Random().nextInt(chromosome.getGeneLength() / 10 + 1) + 1;
 			int count = 0;
-			
+
 			if (position >= chromosome.getGeneLength() - mutateInterval) {
 				mutateInterval = chromosome.getGeneLength() - position; // Mutate at the last part of the chromosome
 				while (mutateInterval > count) { // Mutate
-					mutation = mutation + 
-							(chromosome.getSequence().charAt(position + mutateInterval - 1) == '0' ? "1" : "0");
+					mutation = mutation
+							+ (chromosome.getSequence().charAt(position + mutateInterval - 1) == '0' ? "1" : "0");
 					++count;
 				}
 				temp = chromosome.getSequence().substring(0, position) + mutation;
-			}
-			else {
+			} else {
 				while (mutateInterval > count) { // Mutate
-					mutation = mutation + 
-							(chromosome.getSequence().charAt(position + mutateInterval - 1) == '0' ? "1" : "0");
+					mutation = mutation
+							+ (chromosome.getSequence().charAt(position + mutateInterval - 1) == '0' ? "1" : "0");
 					++count;
 				}
 				if (position == 0) // Mutate at the head part of the chromosome
 					temp = mutation + chromosome.getSequence().substring(position + mutateInterval);
-				else if(position + mutateInterval + 1 == chromosome.getGeneLength()) // The last part of the chromosome is a character
-					temp = chromosome.getSequence().substring(0, position) + mutation + 
-					chromosome.getSequence().charAt(chromosome.getGeneLength() - 1);
+				else if (position + mutateInterval + 1 == chromosome.getGeneLength()) // The last part of the chromosome
+																						// is a character
+					temp = chromosome.getSequence().substring(0, position) + mutation
+							+ chromosome.getSequence().charAt(chromosome.getGeneLength() - 1);
 				else // Mutate at the middle part of the chromosome
-					temp = chromosome.getSequence().substring(0, position) + mutation + 
-					chromosome.getSequence().substring(position + mutateInterval);
+					temp = chromosome.getSequence().substring(0, position) + mutation
+							+ chromosome.getSequence().substring(position + mutateInterval);
 			}
-			
+
 			// Update the chromosome and the fitness of the individual
 			chromosome.setSequence(temp);
 			this.fitness = TestGeneticAlgorithm.getFunctionValue(chromosome.getDecimalX(), chromosome.getDecimalY());
 		}
+	}
+
+	@Override
+	public int compareTo(Individual o) {
+		return fitness.compareTo(o.getFitness());
 	}
 
 }
